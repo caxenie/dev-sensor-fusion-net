56671368c57b33ca80348e1ca0747c73e3adb1f8
Cristian Axenie <cristian.axenie@gmail.com>
2014-06-23 21:14:24 +0200

    Added support for c/matlab runtime and analysis on different runtime dataset format.

--- corr_learn_net.m
+++ corr_learn_net.m
@@ -8,7 +8,8 @@
 clear all; close all; clc; pause(2);
 %% LOAD DATA AND SETUP RUNTIME
 % -------------- simulation options parametrization ---------------------
-simopts.mode            = 'analyze';                      % mode given the function of the script, i.e. run, analyze
+simopts.mode            = 'analyze';                  % mode given the function of the script, i.e. run, analyze
+simopts.environment     = 'c';                        % runtime environment {c, matlab}
 simopts.debug.verbose   = 0;                          % flag to activate / inactivate debug verbose
 simopts.debug.visual    = 0;                          % flag to activate / inactivate debug visualization
 % ---------- data generation and preprocessing parametrization ----------
@@ -18,22 +19,23 @@ simopts.data.slidesize  = 1;                          % sliding window size (onl
 simopts.data.numsamples = 1000;                       % total number of samples to generate (only for generated data)
 simopts.data.trainvsize = 5;                          % size (in samples) of the input vector (only for sliding / interval / hunt )
 simopts.data.ntrainv    = 100;                        % number of train vectors for training the network (only for full dataset)
-simopts.data.corrtype   = 'algebraic';                % input data correlation type, i.e. algebraic, temporal, nonlinear, 
-                                                      % delay (sine waves only for generated), amplitude (sine waves only for generated)
+simopts.data.corrtype   = 'algebraic';                % input data correlation type, i.e. algebraic, temporal, nonlinear,
+% delay (sine waves only for generated), amplitude (sine waves only for generated)
 % ---------------------- parametrize the network ------------------------
 simopts.net.sizex       = 10;                         % sizex x sizey lattice SOM nets
 simopts.net.sizey       = 10;                         % X - rows , Y - cols
 simopts.net.params      = 'fixed';                    % adaptive processes parameters, i.e. fixed/adaptive
 simopts.net.alpha       = 0.01;                       % initial learning rate (adaptive process)
 simopts.net.sigma       = max(simopts.net.sizex, ...
-                              simopts.net.sizey)/2+1; % initial neighborhood size (adaptive process)
+    simopts.net.sizey)/2+1; % initial neighborhood size (adaptive process)
 simopts.net.maxepochs   = 500;                        % number of epochs to train
 simopts.net.gamma       = 0.1;                        % cross-modal activation impact on local som learning
 simopts.net.xi          = 0.02;                        % inhibitory component in sensory projections weight update
 simopts.net.kappa       = 0.2;                        % learning rate (gain factor) in Hebbian weight update
 simopts.net.lambda      = simopts.net.maxepochs/...
-                          log(simopts.net.sigma);     % temporal coef
+    log(simopts.net.sigma);     % temporal coef
 simopts.net.xmodlearn   = 'hebb';                     % cross modal learning mechanism, i.e. hebb, covariance (pseudo-Hebbian) or none (no cross interaction)
+% http://icwww.epfl.ch/~gerstner/SPNM/node72.html
 simopts.net.synapses    = 'genesis';                   % initial state for sensory afferents synapses (genesis (=0) or random (=[min/, max]) or fixed (={v1,v2})
 %% RUN THE CORRELATION LEARNING NETWORK (MODES: RUN / ANALYZE)
 % check mode
@@ -47,17 +49,24 @@ switch(simopts.mode)
         % run the network
         runtime_data = cln_iterate_network(simopts, netin, som1, som2);
     case 'analyze'
-        % parametrize the simulation according the data file
-        simopts.data.infile = sprintf('%d_epochs_%d_x_%d_neurons_%s_source_data_%s_correlation_%d_trainvsize_%d_trainvnum_%s_params_%s_xmodlearn',...
-            simopts.net.maxepochs, simopts.net.sizex, simopts.net.sizey, simopts.data.source, simopts.data.corrtype, simopts.data.trainvsize,...
-            simopts.data.ntrainv, simopts.net.params, simopts.net.xmodlearn);
-        % prepare input data for visualization
-        visin = cln_runtime_dataset_setup(simopts);
-        if(isempty(fieldnames(visin))==1) 
-            return; % no net runtime data file present
+        % check runtime environment
+        switch simopts.environment
+            case 'matlab'
+                % parametrize the simulation according the data file
+                simopts.data.infile = sprintf('%d_epochs_%d_x_%d_neurons_%s_source_data_%s_correlation_%d_trainvsize_%d_trainvnum_%s_params_%s_xmodlearn',...
+                    simopts.net.maxepochs, simopts.net.sizex, simopts.net.sizey, simopts.data.source, simopts.data.corrtype, simopts.data.trainvsize,...
+                    simopts.data.ntrainv, simopts.net.params, simopts.net.xmodlearn);
+                % prepare input data for visualization
+                visin = cln_runtime_dataset_setup(simopts);
+                if(isempty(fieldnames(visin))==1)
+                    return; % no net runtime data file present
+                end
+                % visualize network dynamics
+                cln_visualize(visin);
+            case 'c'
+                % read the binary file generated by the c network code
+                simopts.data.infile = '';
         end
-        % visualize network dynamics
-        cln_visualize(visin);
         fprintf(1, 'Finalized visualization process\n');
 end
-% end simulation 
+% end simulation
